788a200233fd50af3cf362017c3264b8c0f40d9f
ekiauhce <ekiauhce@yandex.ru>
2021-01-17 18:24:02 +0300

    Change project structure

--- set_env.sh
+++
@@ -1,6 +0,0 @@
-#Нужно прописать сюда путь до венвы,
-#добавить PYTHONPATH в $PATH
-#и вписать апи токен
-. ~/code/python/ruthelp_bot/env/bin/activate
-export PYTHONPATH="~/code/python/ruthelp_bot/"
-export TG_API_TOKEN="TOKEN"
