ac1a767a7c1df56d2db240ff2dfcec2b1a6992d6
ekiauhce <ekiauhce@yandex.ru>
2021-01-20 12:04:48 +0300

    Refactoring

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,12 +1,1 @@
-from .db import (
-    create_categories,
-    get_categories_list,
-    create_applications,
-    insert_application,
-    create_directors,
-    get_director,
-    get_applications,
-    get_applications_field_names,
-    set_application_ok,
-    init
-)
+from .db import *
