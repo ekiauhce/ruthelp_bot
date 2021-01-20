84577dff20807e54192cbb597f717c2887d493e7
ekiauhce <ekiauhce@yandex.ru>
2021-01-20 17:25:20 +0300

    Change scripts

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
-. env/bin/activate
+. env/bin/activate || exit
 
 # Change us!
 export TG_API_TOKEN=
 export HOST_IP=
 
 cd ./src/ || exit
-python init_db.py || exit
-python main.py
+python init_db.py && python main.py
+cd ..
 
 unset TG_API_TOKEN
+unset HOST_IP
 
 deactivate
