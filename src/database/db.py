bc421aeb3dc4e384e5898941dc83c49fe230cfe1
ekiauhce <ekiauhce@yandex.ru>
2021-01-20 20:38:18 +0300

    Docs

--- src/database/db.py
+++ src/database/db.py
@@ -92,7 +92,7 @@ def delete_admin(chat_id: int) -> bool:
 
 
 def init() -> None:
-    """Создает схему бд, где нужно добавляет данные. Функция переписывает данные в бд!!!"""
+    """Создает схему бд, где нужно добавляет данные. Функция не переписывает данные в бд!!!"""
     create_categories()
     create_applications()
     create_directors()
